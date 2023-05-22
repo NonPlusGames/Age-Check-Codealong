3d344a18afbfa62c03e728c275cc47af6c022461
Rafael <rim223@live.com>
2023-05-22 14:36:51 -0400

    Code Along Template

---
+++ main.py
@@ -0,0 +1,20 @@
+# You will be coding an age check program.
+# The program should do all of the following:
+#  - Ask the user for their age
+#  - Store that age in a variable
+#  - Print out whether or not they are old enough to vote
+# -  Stretch - Check whether or not they are old enough to drive, rent a car, or get a senior discount
+
+# Ask and store age into a variable using input()
+
+
+# Check if they are old enough to vote using if/else
+
+
+    # If they are, print "You are old enough to vote!"
+
+
+    # If they are not, print "You are not old enough to vote!"
+
+
+# Stretch Challenges
